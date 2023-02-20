58dd9a05bb43b4d0b6272cb31759a6c03fd9c319
Николай Николаенко <nikonikolaenko@yandex.ru>
2023-02-20 16:43:46 +0300

    second review

--- src/components/Swither/Swither.jsx
+++ src/components/Swither/Swither.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
 import styles from './styles/style.module.css';
+import PropTypes from 'prop-types';
 
 export function Switcher({ bun, sauce, mains }) {
+  Switcher.propTypes = {
+    bun: PropTypes.bool.isRequired,
+    sauce: PropTypes.bool.isRequired,
+    mains: PropTypes.bool.isRequired,
+  };
+
   return (
     <div className={styles.switcher}>
       <Tab value="one" active={bun}>
